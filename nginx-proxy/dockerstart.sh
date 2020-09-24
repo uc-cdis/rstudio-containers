e5303337b727916d15721ed34d34eec83fdcec61
Reuben Pasquini <reuben@frickjack.com>
2020-09-24 13:44:32 -0500

    chore(nginx-proxy): add REDIRECT_PREFIX

--- nginx-proxy/dockerstart.sh
+++ nginx-proxy/dockerstart.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # Setup nginx.conf
-IN_PREFIX="${IN_PREFIX:-/lw-workspace/proxy/}"
-APP_PREFIX="${APP_PREFIX:-/}"
+REDIRECT_PREFIX="${REDIRECT_PREFIX:-""}"
+REDIRECT_PREFIX="${REDIRECT_PREFIX%/}"
+IN_PREFIX="${IN_PREFIX:-lw-workspace/proxy/}"
+IN_PREFIX="${IN_PREFIX#/}"
+APP_PREFIX="${APP_PREFIX:-""}"
+APP_PREFIX="${APP_PREFIX#/}"
 APP_PORT="${APP_PORT:-80}"
 
-sed -s "s@IN_PREFIX@$IN_PREFIX@g" /etc/nginx/nginx.conf.template \
+sed -s "s@REDIRECT_PREFIX@$REDIRECT_PREFIX@g" /etc/nginx/nginx.conf.template \
+   | sed -s "s@IN_PREFIX@$IN_PREFIX@g" \
    | sed -s "s@APP_PREFIX@$APP_PREFIX@g" \
    | sed -s "s@APP_PORT@$APP_PORT@g" \
    | tee /etc/nginx/nginx.conf
